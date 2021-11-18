a835aca214f8cac03eb024c509fccf59643b052e
Nilesh Ramteke <nilesh.ramteke@mastercard.com>
2021-11-18 22:29:19 +0530

    Initial Commit

---
+++ WarCardGameSwiftUI/WarCardGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameSwiftUIApp.swift
+//  WarCardGameSwiftUI
+//
+//  Created by Nilesh Ramteke on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
